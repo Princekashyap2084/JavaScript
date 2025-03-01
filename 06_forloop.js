2128ec70c5ead8b29b36ae25f32162b19e03d61f
Amit Kumar Raikwar <88871643+Princekashyap2084@users.noreply.github.com>
2025-03-01 18:21:20 +0000

    check for in loop

---
+++ 06_forloop.js
@@ -0,0 +1,36 @@
+// aaj ham baat karege for, for in, and for of loop ke bare me 
+// loop ka matlab hota hai koi cheej repet hona 
+
+// for loop ham jab use karte hai jab hame kisi cheej ko bar bar print karana ho tab ham for loop use karte hai 
+
+// for example :- 
+
+// agar mereko apna naam 10 times print karna hai to me bar bar console log nahi karuga agar maan lo mene agar 10 bar kar v liya console log fir v agar 100 bar karna pada to 100 baar thodi likhuga is liye ham for loop use karege 
+
+// for (let i = 0; i < 5; i++) {
+//     const fullName = console.log("Amitkumarraikwar");
+// }
+
+// ek or example dhekte hai 
+// isme hame 0 se 100 tak counting print karni hai
+
+// for (let i = 0; i < 101; i++) {
+//     console.log(i);
+// }
+
+// ab isme hamne ek variable banaya aap kuch v naam rakh sakte ho jo v aapko rakhna ho lakin log mostily i use karte hai because i ka matlab hota hai iteration.
+
+// ab aap dhek sakte ho ki isme i = 0 diya iska matlab hai ki 0 se start karo  or  jab tak i < 5 i less than 5 hai jab tak i++ karte jao means 0 me 1 add karte jao or har ek iteration par naam print karte jao to loop 5 times chalega or 5 times naam print karega.
+
+// ab aate hai for in loop par 
+
+// for in ham jab use karte hai jab hame pass arrya hai numbers ki us par for lagane ke liye ek example ke saath dhekte hai 
+
+// const person = {fname:"John ", lname:"Doe ", age:25};
+
+// let text = "";
+// for (let x in person) {
+//   text += person[x];
+//   let a = text;
+//   console.log(a);
+// }
