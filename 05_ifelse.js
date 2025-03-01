f2caafc099bc81904506a1e00971cc62f14dc584
Amit Kumar Raikwar <88871643+Princekashyap2084@users.noreply.github.com>
2025-03-01 16:22:49 +0000

    check if else statement

---
+++ 05_ifelse.js
@@ -0,0 +1,16 @@
+// isme baat karege if else statement ke bare me if else wahi hai jo hame daily life me or english or math me padte the ki agar ye hua to ye karo or agar ye nahi hua to ye kardo 
+// isko ham ange bhuat use karege
+
+// for example :- 
+const score = 0;
+let finalScore = 100; 
+if (score === finalScore) {
+    console.log("yes, both are equal");
+} else {
+    console.log("No, both are not equal");
+}
+
+
+// isme ek if or else ke andar v if else likh sakte hai or if ke baad v ek or if use karke dusri conditon likh sakte hai
+
+// abhi itna hi hai or kuch hua to add kar duga 
