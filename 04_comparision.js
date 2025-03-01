2edaeaff84261089750d35b3bcd01dc2c3320b28
Amit Kumar Raikwar <88871643+Princekashyap2084@users.noreply.github.com>
2025-03-01 15:54:37 +0000

    do comprision

---
+++ 04_comparision.js
@@ -0,0 +1,13 @@
+// dheko javascript or jitni v lang hoti hai like c, c++, java and python sari lang me comparision operators hote hai lakin java script me kuch extra comparision operators hai or isme thode alag tarike se comparision hota hai
+
+// dheko greater than, less than, and, or, not ye sare operator hai jo ham phele math me use karte the wahi to ab example or dhek lete hai 
+// for example :-
+
+// console.log(10 > 9);            // output true   isme ham dhek rahe hai ki kya 10 bada hai 9 se or iska answer hai haa bada hai to return aaya true
+// console.log(10 < 9);            // output false  isme ham dhek rahe hai ki kya 10 chota hai 9 se or iska answer hai nahi bada hai to return aaya false
+// console.log(10 >= 9);           // output true   isme ham dhek rahe hai ki kya 10 bada ya equea hai 9 se or iska answer hai haa bada hai to return aaya true
+// console.log(10 <= 9);           // output false  isme ham dhek rahe hai ki kya 10 chota ya equal hai 9 se or iska answer hai nahi bada hai to return aaya false
+// console.log(10 != 9);           // output true   isme ham dhek rahe hai ki kya 10 equal nahi hai 9 se or iska answer hai haa nahi hai to return aaya true
+// console.log(10 > 9 && 10 < 9);  // output false  isme ham do condition check kar rahe hai or jab dono condition sahi hogi tab hi true return karega true
+// console.log(10 > 9 || 10 < 9);  // output true   isme ham do condition check kar rahe hai or jab dono condition me se koi ek condition sahi hona chiye to answer true aa jyega 
+
