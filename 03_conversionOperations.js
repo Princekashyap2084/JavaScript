74a08a8786927743017754ea07046bb576b93a7c
Amit Kumar Raikwar <88871643+Princekashyap2084@users.noreply.github.com>
2025-03-01 14:27:13 +0000

    operations

--- 03_conversionOperations.js
+++ 03_conversionOperations.js
@@ -17,3 +17,24 @@
 // console.log(typeof stringNumber);
 
 // bas abhi to mereko itna hi aata hai agaar kuch or seekha to add karduga 
+
+// ab Operations or bata deta hu means jo ham basic math karte aa rahe hai like (add, subtract, multiply, division, exponnent, modulas).Operations.
+// for example :-
+
+// console.log(2 + 5)  // addition
+// console.log(2 - 5)  // subtraction
+// console.log(2 * 5)  // multiply
+// console.log(2 / 5)  // divide
+// console.log(2 % 5)  // modulas
+// console.log(2 ** 5) // exponents
+
+// agar ab ham string or number ko saath me add kare to kya hoga wo dhekte hai 
+
+// console.log("2" + 2);           // output 22
+// console.log("2" + 2 + "2");     // output 222
+// console.log(2 + "2");           // output 22
+// console.log("2" + "2");         // output 22
+// console.log("amit" + 2);        // output amit2
+// console.log(2 + "amit");        // output 2amit
+
+// bs abhi tak to itna hi tha agar kuch or hota hai to likh duga means update karduga 
