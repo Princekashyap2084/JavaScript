209cb9bc6a7e7f57dc3dc151b32ed30217bd2138
Amit Kumar Raikwar <88871643+Princekashyap2084@users.noreply.github.com>
2025-03-01 13:54:26 +0000

    convert number into string

--- 03_conversionOperations.js
+++ 03_conversionOperations.js
@@ -1,4 +1,4 @@
-// is file me batauga ki kisi datatype ko dusre datatype me convert kaise kare jaise ki string ko number me.
+// is file me batauga ki kisi datatype ko dusre datatype me convert kaise kare jaise ki string ko number me or number se string me.
 
 // String => number
 
@@ -7,5 +7,13 @@
 
 // let numAge = Number(rupees);
 // console.log(typeof(numAge));
+
+// number => string
+
+// let someNumber = 26
+
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
 // bas abhi to mereko itna hi aata hai agaar kuch or seekha to add karduga 
