a3f487e5b4a43e5f1398415330003cab168837db
Amit Kumar Raikwar <88871643+Princekashyap2084@users.noreply.github.com>
2025-03-01 09:01:04 +0000

    initial commit

---
+++ 01_Basic.js
@@ -0,0 +1,31 @@
+"usestate"
+
+// JavaScript me 3 variable me ham data store kar sakte hai 
+
+// var, const, let
+
+// var me agar ham kuch v store karte hai to usko ham fir kahi par v use kar sakte hai means scope oriented nahi hai to ab hai new javascript me var ko use nahi karna chiye kyuki ye bhuat kharab hota hai 
+// for exampel :- 
+
+// var a = 3
+// var a = 5;
+
+// console.log(a);
+
+// const me ham jo v store karte hai usko ham change nahi kar sakte 
+
+// for example :-
+// const fullName = "Amit Kumar Raikwar";
+// console.log(fullName);
+// const fullName = "Prince Kashyap";
+// console.log(fullName);
+
+// let me ham jo store karte hai usko ham change kar sakte hai lakin sirf scope ke andar hi change kar sakte hai uske bahar nahi 
+// for example :- 
+
+// let fullName = "Amit Kumar Raikwar";
+// fullName = "princekashyap";
+// console.log(fullName);
+
+// ham start me means script ke top par ek "usestate" word likhte hai jo batata hai ki ham new javascript use kar rahe hai purani nahi 
+
---
+++ 02_datatypes.js
@@ -0,0 +1,54 @@
+// JavaScript me bhuat kaam datatypes hote hai usme se kuch datatype hai 
+
+
+// String
+// Number
+// BigInt
+// Boolean
+// undefined
+// null
+// Symbol
+// Object
+
+
+// string ko ham double or single quotes me likhte hai. for example :- 
+// console.log('Amitkumarraikwar')
+// console.log("princekashyap")
+// let color = "Yellow";
+// let lastName = "Johnson";
+// console.log(color);
+// console.log(lastName);
+
+// Number ka matlab hai koi v positive or negative number or jo decimal me v hote hai wo v number me aate hai like 2, 3, -41, 5.4, 456.58 and etc.. 
+// for example :-
+
+// let a = 5.45;
+// console.log(a);
+// const age = 21;
+// console.log(age); 
+
+
+// BigInt ka ham jab use karte hai jab ham number bhuat bada ho vaise abhi ye hamare kisi use ka nahi hai to isko yahi drop karte hai 
+
+// Boolean output me return karta hai true ya false. jaise koi condition sahi hai ya glat us par ham Boolean use karte hai for example :- 
+
+// let a = Boolean(10 > 9);
+// console.log(a);
+
+// undefined ka matlab hai ki kisi variable ko declare kar diya lakin initialized nahi kiya matlab us variable me kuch v store nahi kiya to hame output me undefined milta hai
+
+// for example :- 
+// let lastName;
+// console.log(lastName);
+
+
+// null ka matlab hota hai empty means jab hame variable me kuch value nahi deni hoti to ham usme null likh dete hai.
+
+// for example :- 
+// let declaredVariable;
+// console.log(declaredVariable); // Output: undefined
+
+// let assignedNull = null;
+// console.log(assignedNull); // Output: null
+
+Symbol
